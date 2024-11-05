d32fb374e11982745e8ce5af1a9e0337fbfda11b
Ayush Sinha <ayushs@testvagrant.com>
2024-11-05 13:51:50 +0530

    feat: Implemented the InvoiceGenerate class

--- src/ECOMS/InvoiceGenerator.java
+++ src/ECOMS/InvoiceGenerator.java
@@ -1,4 +1,22 @@
 package ECOMS;
 
 public class InvoiceGenerator {
+    /***
+     * Members
+     * 1. total_cost -> Sum of all the product price with their respective quantity
+     */
+    private double total_cost = 0;
+
+    public void generate_invoice(Order order){
+        System.out.println("No.    Name        Quantity    Price     Cost");
+        int count = 1;
+        for(Product product: order.get_products()){
+            int quantity = product.getStock();
+            double price = product.getPrice();
+            total_cost += price * quantity;
+            System.out.println(count + ". " + product.getName() + " " + quantity + " " + price);
+        }
+        System.out.println("------------------------------------------------------------");
+        System.out.println("Total Price -> " + total_cost);
+    }
 }
--- src/ECOMS/Order.java
+++ src/ECOMS/Order.java
@@ -1,4 +1,20 @@
 package ECOMS;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private final List<Product> productList;
+
+    public Order(){
+        productList = new ArrayList<>();
+    }
+
+
+    public void add_product(Product product){
+        productList.add(product);
+    }
+    public List<Product> get_products() {
+        return productList;
+    }
 }
