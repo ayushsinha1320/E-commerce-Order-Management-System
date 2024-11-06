585e701c29d0dba2c2d005dfd9339f332d260121
Ayush Sinha <ayushs@testvagrant.com>
2024-11-06 15:24:17 +0530

    feat: Added Customer object in Order Class

--- src/ECOMS/Order.java
+++ src/ECOMS/Order.java
@@ -5,16 +5,22 @@ import java.util.List;
 
 public class Order {
     private final List<Product> productList;
+    private final Customer customer;
 
-    public Order(){
+    public Order(Customer customer){
+        this.customer = customer;
         productList = new ArrayList<>();
     }
-
 
     public void add_product(Product product){
         productList.add(product);
     }
+
     public List<Product> get_products() {
         return productList;
+    }
+
+    public Customer getCustomer() {
+        return customer;
     }
 }
